158b2b244bcc51d6fbd561d27e20ae93bc5bd670
Bisrat Kassie <107273623+bisrat415@users.noreply.github.com>
2023-06-14 00:40:11 -0400

    solution added for Odd Even List

---
+++ OddEvenList.java
@@ -0,0 +1,37 @@
+// The is a leetcode question
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+public class OddEvenList {
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null || head.next.next == null) {
+            return head;
+        }
+        ListNode odd = head;
+        ListNode evenHead = head.next;
+        ListNode even = head.next;
+        while (odd.next != null && even.next != null) {
+            odd.next = odd.next.next;
+            even.next = even.next.next;
+            odd = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+        // Time Complexity: O(n) because we are visiting each element in the linked list once
+        // Space Complexity: O(1) because we are only using constant extra space
+    }
+
+}
